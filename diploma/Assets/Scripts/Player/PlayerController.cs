52eca7b95d341f0bb7db27fe88727797de10acbd
Daria <dashakoshelek@gmail.com>
2026-02-21 17:36:52 +0300

    FP Camera

--- diploma/Assets/Scripts/Player/PlayerController.cs
+++ diploma/Assets/Scripts/Player/PlayerController.cs
@@ -1,30 +1,60 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] private float _moveSpeed = 5f;
     [SerializeField] private float _jumpForce = 2f;
-    [SerializeField] private float _gravityScale = -9.8f;
+    [SerializeField] private float _gravity = -9.8f;
+
+    [Header("References")]
+    [SerializeField] private CinemachineCamera _cinCam;
     [SerializeField] private GameEvent onDoorOpened;
+
+    private CharacterController _controller;
 
     private Vector2 _moveInput;
     private Vector3 _velocity;
 
-    private CharacterController _characterController;
-
-    private void Start()
+    private void Awake()
     {
-        _characterController = GetComponent<CharacterController>();
+        _controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    void Update()
+    private void Update()
     {
-        Vector3 move = new Vector3(_moveInput.x, 0, _moveInput.y);
-        _characterController.Move(move * _moveSpeed * Time.deltaTime);
+        HandleRotation();
+        HandleMovement();
+        HandleGravity();
+    }
 
-        _velocity.y += _gravityScale * Time.deltaTime;
-        _characterController.Move(_velocity * Time.deltaTime);
+    private void HandleRotation()
+    {
+        float cameraY = _cinCam.transform.eulerAngles.y;
+        transform.rotation = Quaternion.Euler(0f, cameraY, 0f);
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 move =
+            transform.right * _moveInput.x +
+            transform.forward * _moveInput.y;
+
+        _controller.Move(move * _moveSpeed * Time.deltaTime);
+    }
+
+    private void HandleGravity()
+    {
+        if (_controller.isGrounded && _velocity.y < 0)
+            _velocity.y = -2f;
+
+        _velocity.y += _gravity * Time.deltaTime;
+        _controller.Move(_velocity * Time.deltaTime);
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -34,9 +64,9 @@ public class PlayerController : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (context.performed && _characterController.isGrounded)
+        if (context.performed && _controller.isGrounded)
         {
-            _velocity.y = Mathf.Sqrt(_jumpForce * -2f * _gravityScale);
+            _velocity.y = Mathf.Sqrt(_jumpForce * -2f * _gravity);
             onDoorOpened?.Raise();
         }
     }
